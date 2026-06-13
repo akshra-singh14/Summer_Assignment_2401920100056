98d289d7b7787156d64a3bb3dc18a45bc4b63c8e
akshra-singh14 <akshra1.singh56@gmail.com>
2026-06-13 17:31:45 +0530

    Create Test.java

---
+++ Week _2/OOPs/live/Test.java
@@ -0,0 +1,18 @@
+package live;
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+public class Test {
+    public static void main(String[] ar) {
+        Veena veena = new Veena();
+        veena.play();
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        Playable p;
+        p = veena;
+        p.play();
+        p = saxophone;
+        p.play();
+    }
+}

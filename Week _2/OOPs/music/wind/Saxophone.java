c6233842cdb5c4975abf913893c719a63524dab2
akshra-singh14 <akshra1.singh56@gmail.com>
2026-06-13 17:33:47 +0530

    Create Saxophone.java

---
+++ Week _2/OOPs/music/wind/Saxophone.java
@@ -0,0 +1,8 @@
+package music.wind;
+import music.Playable;
+public class Saxophone implements Playable {
+    @Override
+    public void play() {
+        System.out.println("Now Saxophone is playing.");
+    }
+}
